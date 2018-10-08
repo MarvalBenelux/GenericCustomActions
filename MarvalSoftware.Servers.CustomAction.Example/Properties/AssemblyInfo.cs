a955546cf1b8e156014651cbef08f48c614ad725
Jasper Boot <jasper.boot@marval-benelux.nl>
2018-10-08 20:43:53 +0200

    Normalise AssemblyInfo in Example CustomAction

--- MarvalSoftware.Servers.CustomAction.Example/Properties/AssemblyInfo.cs
+++ MarvalSoftware.Servers.CustomAction.Example/Properties/AssemblyInfo.cs
@@ -1,27 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MarvalSoftware.Servers.CustomActionExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marval Software Ltd")]
+[assembly: AssemblyProduct("Marval Service Management")]
+[assembly: AssemblyCopyright("Copyright Marval Software Ltd 2007-2018")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fcdd2b07-5496-406e-8f6d-35cf3d11fe7c")]
 
-[assembly: AssemblyCompany("Marval Software Ltd")]
-[assembly: AssemblyProduct("Marval Service Management")]
-[assembly: AssemblyCopyright("Copyright Marval Software Ltd 2007-2018")]
-[assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
