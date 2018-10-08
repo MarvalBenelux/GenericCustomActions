7c722bc5b0ef54de769dd5179ae97effea323f61
Jasper Boot <jasper.boot@marval-benelux.nl>
2018-10-08 21:43:18 +0200

    ExecuteAgent: Add generic CustomAction to execute executables/scripts
    This is a generic custom action to execute an agent / executable /
    script with supplied parameters.
    
    ```xml
        @{
    
            string monitoringId = "";
            foreach (var item in Model.Attributes)
            {
                if (item.Type.Identifier == 1252)
                {
                    monitoringId = item.Value;
                }
            }
    
            string requestNumber = @Model.RequestNumber;
    
        }<AgentActionMessage integrationName="MonitoringTool - Event closed">
            <AgentFullPath>D:\Interfaces\MonitoringTool\report_event_closed.bat</AgentFullPath>
            <Parameters>
                <Parameter>/id="@monitoringId"</Parameter>
                <Parameter>/message="Event @requestNumber has been closed in MSM."</Parameter>
            </Parameters>
        </AgentActionMessage>
    ```

---
+++ MarvalSoftware.Servers.CustomAction.ExecuteAgent/ExecuteAgentAction.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using MarvalSoftware.Data;
+using MarvalSoftware.ExceptionHandling;
+using Serilog;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using MarvalSoftware.Extensions;
+
+namespace MarvalSoftware.Servers.CustomAction.ExecuteAgent
+{
+    public class ExecuteAgentAction : IIntegrationAction
+    {
+        #region Implementation of IIntegrationAction
+
+        /// <summary>
+        /// Providing a name for your custom action is Required
+        /// </summary>
+        public ExecuteAgentAction()
+        {
+            this.ActionName = "ExecuteAgent";
+        }
+
+        /// <summary>
+        /// The integration action name
+        /// </summary>
+        public string ActionName { get; set; }
+
+        /// <summary>
+        ///     Executes the action, executing an agent with the supplied parameters.
+        /// </summary>
+        /// <param name="sessionId">The session Id of the user executing this action.</param>
+        /// <param name="integrationMessage">Accepts a string integration message that can be deserialized into the required class.</param>
+        public void Execute(string sessionId, string integrationMessage)
+        {
+            string agentIntegration = "Unknown Agent Integration";
+            string agentFullPath = "<unknown>";
+            try
+            {
+                AgentActionMessage executeAgentMessage = integrationMessage.XmlDeserialize<AgentActionMessage>();
+                agentIntegration = executeAgentMessage.IntegrationName ?? "Agent Integration";
+                agentFullPath = executeAgentMessage.AgentFullPath;
+                if (! File.Exists(agentFullPath))
+                {
+#if DEBUG
+                    // send a message to the UI
+                    SendMessage(this, new MessageEventArgs(sessionId, new TrayMessage
+                    {
+                        Heading = agentIntegration,
+                        Text = String.Format("Agent executable not found: {0}", agentFullPath),
+                        Type = TrayMessage.MessageTypes.Warning
+                    }));
+#endif
+                    var logException = new MarvalApplicationException(String.Format(" Agent executable not found: {0} ", agentFullPath));
+                    Log.Error(logException, String.Format("{0}: {1}: {2}", this.ActionName, agentIntegration, logException.Message));
+                    return;
+                }
+                var agentExecution = ExecuteAgent(agentFullPath, executeAgentMessage.Parameters.Select(p => p.Value).ToArray());
+                agentExecution.Wait(10000);
+                if (agentExecution.IsFaulted)
+                {
+                    throw agentExecution.Exception;
+                }
+                var result = agentExecution.Result;
+                var debugInfo = new MarvalApplicationException(String.Format("Success: {0} ", result));
+                Log.Debug(debugInfo, String.Format("{0}: {1}: {2}", this.ActionName, agentIntegration, debugInfo.Message));
+#if DEBUG
+                // send a message to the UI
+                SendMessage(this, new MessageEventArgs(sessionId, new TrayMessage
+                {
+                    Heading = agentIntegration,
+                    Text = "Successful agent execution",
+                    Type = TrayMessage.MessageTypes.Information
+                }));
+#endif
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, String.Format("{0}: {1}: {2}", this.ActionName, agentIntegration, (e.InnerException ?? e).Message));
+                if ((e is AggregateException) && (e.InnerException != null))
+                {
+                    ExceptionHandler.Publish(new MarvalApplicationException(String.Format("[{0}] {1}: Error while executing {2}", this.ActionName, agentIntegration, agentFullPath ?? "<unset>"), e.InnerException));
+                }
+                else
+                {
+                    ExceptionHandler.Publish(new MarvalApplicationException(String.Format("[{0}] {1}: Exception in agent integration", this.ActionName, agentIntegration), e));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Public event for sending a message through to the UI
+        /// </summary>
+        public event EventHandler<MessageEventArgs> SendMessage = delegate { };
+
+        #endregion
+
+        #region Public implementation details
+
+        public struct Parameter
+        {
+            [XmlText]
+            public string Value { get; set; }
+        }
+        public class AgentActionMessage
+        {
+            [XmlAttribute(AttributeName = "integrationName")]
+            public string IntegrationName { get; set; }
+            public string AgentFullPath { get; set; }
+            public Parameter[] Parameters { get; set; }
+        }
+
+        #endregion
+
+        #region Internals
+
+        private async Task<int> ExecuteAgent(string agentFullPath, string[] parameters)
+        {
+            return await RunProcessAsync(agentFullPath, String.Join(" ", parameters)).ConfigureAwait(false);
+        }
+
+        private static async Task<int> RunProcessAsync(string fileName, string args, bool redirectOutputToConsole = false)
+        {
+            using (var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName,
+                    Arguments = args,
+                    UseShellExecute = !redirectOutputToConsole,
+                    RedirectStandardOutput = redirectOutputToConsole,
+                    RedirectStandardError = redirectOutputToConsole,
+                    CreateNoWindow = true
+                },
+                EnableRaisingEvents = true
+            })
+            {
+                return await RunProcessAsync(process, redirectOutputToConsole).ConfigureAwait(false);
+            }
+        }
+        private static Task<int> RunProcessAsync(Process process, bool redirectOutputToConsole = false)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            process.Exited += (s, ea) => tcs.SetResult(process.ExitCode);
+            if (redirectOutputToConsole)
+            {
+                process.OutputDataReceived += (s, ea) => Console.WriteLine(ea.Data);
+                process.ErrorDataReceived += (s, ea) => Console.WriteLine("ERR: " + ea.Data);
+            }
+
+            bool started = process.Start();
+            //if (!started)
+            //{
+            //    throw new InvalidOperationException("Could not start process: " + process);
+            //}
+
+            if (redirectOutputToConsole)
+            {
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+            }
+
+            return tcs.Task;
+        }
+
+        #endregion
+    }
+
+}
---
+++ MarvalSoftware.Servers.CustomAction.ExecuteAgent/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CustomActionExecuteAgent")]
+[assembly: AssemblyDescription("A custom action plugin to execute an agent / executable / script with supplied parameters")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marval Software Ltd.")]
+[assembly: AssemblyProduct("Marval MSM - Custom Integration Module")]
+[assembly: AssemblyCopyright("Copyright 2017 Marval Software Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bcd7f0c1-a518-4f60-a82d-ad6f2d80ea6a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
