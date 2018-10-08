ba9bf6193983c65345eb59fbf68e6ebf82f3c0ac
Jasper Boot <jasper.boot@marval-benelux.nl>
2018-10-08 22:25:57 +0200

    PostForm: Add generic CustomAction to POST x-form-urlencoded data via HTTP(S)
    Request Action Messages for this generic CustomAction are in XML format
    and look like the following:
    
    ```xml
        <FormPostMessage integrationName="SMS Integration">
            <BaseUrl>http://192.0.2.1/</BaseUrl>
            <PostEndpoint>api/send-sms</PostEndpoint>
            <Headers>
                <Header key="Pragma">no-cache</Header>
                <Header key="X-MSM-Version">@(Model.Assignee.GetType().Assembly.GetName().Version.ToString(3))</Header>
            </Headers>
            <Proxy disable="false">
                <Username>svc-msm-proxy_usr</Username>
                <Password encoded="true">QmFzZTY0X2VuY29kZWRfcGFzc3dvcmQ="</Password>
            </Proxy>
            <FormValues>
                <FormValue key="message_text">Major incident: @Model.FullRequestNumber Description: @Model.Description</FormValue>
                <FormValue key="receiver">@Model.Contact.TelephoneNumber</FormValue>
            </FormValues>
        </FormPostMessage>
    ```

---
+++ MarvalSoftware.Servers.CustomAction.PostForm/PostFormAction.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using MarvalSoftware.Data;
+using MarvalSoftware.ExceptionHandling;
+using MarvalSoftware.Extensions;
+using Serilog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MarvalSoftware.Servers.CustomAction.PostForm
+{
+    public class PostFormAction : IIntegrationAction
+    {
+        #region Implementation of IIntegrationAction
+
+        /// <summary>
+        /// Providing a name for a custom action is Required
+        /// </summary>
+        public PostFormAction()
+        {
+            this.ActionName = "PostForm";
+        }
+
+        /// <summary>
+        /// The integration action name
+        /// </summary>
+        public string ActionName { get; set; }
+
+        /// <summary>
+        ///     Executes the action returning an number of messages.
+        /// </summary>
+        /// <param name="sessionId">The session Id of the user executing this action.</param>
+        /// <param name="integrationMessage">Accepts a string integration message that can be deserialized into the required class.</param>
+        public void Execute(string sessionId, string integrationMessage)
+        {
+            string postIntegration = "Unknown Post Integration";
+            string postBaseAddress = "<unknown>";
+            try
+            {
+                FormPostMessage postIntegrationMessage = integrationMessage.XmlDeserialize<FormPostMessage>();
+                postIntegration = postIntegrationMessage.IntegrationName ?? "Post Integration";
+                postBaseAddress = postIntegrationMessage.BaseUrl;
+                var formValues = postIntegrationMessage.FormValues.ToDictionary(fv => fv.Key, fv => fv.Value);
+                var additionalHeaders = postIntegrationMessage.Headers.ToDictionary(h => h.Key, h => h.Value);
+                var formPost = PostForm(new Uri(postBaseAddress), postIntegrationMessage.PostEndpoint, formValues, additionalHeaders, ParseWebProxyInfo(postIntegrationMessage.Proxy));
+                formPost.Wait(10000);
+                if (formPost.IsFaulted)
+                {
+                    throw formPost.Exception;
+                }
+                var result = formPost.Result;
+                var debugInfo = new MarvalApplicationException(String.Format("Success: {0} ({1}) - {2}", (int)result.StatusCode, result.StatusCode, result.ReasonPhrase));
+                Log.Debug(debugInfo, String.Format("{0}: {1}: {2}", this.ActionName, postIntegration, debugInfo.Message));
+#if DEBUG
+                // send a message to the UI
+                SendMessage(this, new MessageEventArgs(sessionId, new TrayMessage
+                {
+                    Heading = postIntegration,
+                    Text = "Successful postback",
+                    Type = TrayMessage.MessageTypes.Information
+                }));
+#endif
+            }
+            catch (HttpRequestException e)
+            {
+                Log.Error(e, String.Format("{0}: {1}: {2}", this.ActionName, postIntegration, e.Message));
+                ExceptionHandler.Publish(new MarvalApplicationException(String.Format("[{0}] {1}: Unable to connect with {2}", this.ActionName, postIntegration, postBaseAddress ?? "<unset>"), e));
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, String.Format("{0}: {1}: {2}", this.ActionName, postIntegration, (e.InnerException ?? e).Message));
+                if ((e is AggregateException) && (e.InnerException != null))
+                {
+                    ExceptionHandler.Publish(new MarvalApplicationException(String.Format("[{0}] {1}: Error while communicating with {2}", this.ActionName, postIntegration, postBaseAddress ?? "<unset>"), e.InnerException));
+                }
+                else
+                {
+                    ExceptionHandler.Publish(new MarvalApplicationException(String.Format("[{0}] {1}: Exception in Form Post Integration", this.ActionName, postIntegration), e));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Public event for sending a message through to the UI
+        /// </summary>
+        public event EventHandler<MessageEventArgs> SendMessage;
+
+        #endregion
+
+        #region Public implementation details
+
+        public struct Header
+        {
+            [XmlAttribute(AttributeName = "key")]
+            public string Key { get; set; }
+            [XmlText]
+            public string Value { get; set; }
+        }
+
+        public struct FormValue
+        {
+            [XmlAttribute(AttributeName = "key")]
+            public string Key { get; set; }
+            [XmlText]
+            public string Value { get; set; }
+        }
+
+        public struct ProxyUsername
+        {
+            [XmlText]
+            public string Value { get; set; }
+        }
+        public struct ProxyPassword
+        {
+            private string password;
+            [XmlText]
+            public string Value
+            {
+                get
+                {
+                    if (IsEncoded)
+                    {
+                        try
+                        {
+                            return Encoding.UTF8.GetString(Convert.FromBase64String(password));
+                        }
+                        catch
+                        {
+                            return "";
+                        }
+                    }
+                    else
+                    {
+                        return password;
+                    }
+                }
+                set
+                {
+                    if (IsEncoded)
+                    {
+                        try
+                        {
+                            password = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+                        }
+                        catch
+                        {
+                            password = "";
+                        }
+                    }
+                    else
+                    {
+                        password = value;
+                    }
+                }
+            }
+            [XmlAttribute(AttributeName = "encoded")]
+            public bool IsEncoded { get; set; }
+        }
+        public struct ProxyInfo
+        {
+            [XmlAttribute(AttributeName = "disable")]
+            public bool Disabled { get; set; }
+            public ProxyUsername Username;
+            public ProxyPassword Password;
+        }
+
+        public class FormPostMessage
+        {
+            [XmlAttribute(AttributeName = "integrationName")]
+            public string IntegrationName { get; set; }
+            public string BaseUrl { get; set; }
+            public string PostEndpoint { get; set; }
+            public Header[] Headers { get; set; }
+            public ProxyInfo Proxy { get; set; }
+            public FormValue[] FormValues { get; set; }
+        }
+
+        #endregion
+
+        #region Internals
+
+        private class WebProxyInfo
+        {
+            public bool DoNotUseProxy { get; set; }
+            public IWebProxy Proxy { get; set; }
+        }
+
+        private WebProxyInfo ParseWebProxyInfo(ProxyInfo? proxyInfo)
+        {
+            bool doNotUseProxy = false;
+            IWebProxy proxy = null;
+
+            if (proxyInfo != null)
+            {
+                doNotUseProxy = (proxyInfo?.Disabled ?? false);
+                if (! doNotUseProxy)
+                {
+                    proxy = WebRequest.GetSystemWebProxy();
+                    proxy.Credentials = new NetworkCredential(proxyInfo?.Username.Value ?? "", proxyInfo?.Password.Value ?? "");
+
+                }
+            }
+            return new WebProxyInfo()
+            {
+                DoNotUseProxy = doNotUseProxy,
+                Proxy = proxy
+            };
+        }
+
+        private async Task<HttpResponseMessage> PostForm(Uri baseAddress, string postEndpointUrl, Dictionary<string, string> formValues, Dictionary<string, string> additionalHeaders = null, WebProxyInfo proxyInfo = null)
+        {
+            HttpResponseMessage response;
+
+            using (var handler = new HttpClientHandler())
+            {
+                if (proxyInfo?.DoNotUseProxy ?? false)
+                {
+                    handler.UseProxy = false;
+                }
+                else if ((proxyInfo?.Proxy ?? null) != null)
+                {
+                    handler.Proxy = proxyInfo.Proxy;
+                }
+
+                using (var client = new HttpClient(handler))
+                {
+                    client.BaseAddress = baseAddress;
+                    if (additionalHeaders != null)
+                    {
+                        foreach (var additionalHeader in additionalHeaders)
+                        {
+                            client.DefaultRequestHeaders.Add(additionalHeader.Key, additionalHeader.Value);
+                        }
+                    }
+
+                    response = await client.PostAsync(postEndpointUrl, new FormUrlEncodedContent(formValues));
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+            return response;
+        }
+
+        #endregion
+
+    }
+}
---
+++ MarvalSoftware.Servers.CustomAction.PostForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CustomActionPostForm")]
+[assembly: AssemblyDescription("A custom action plugin to post form values to a URL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marval Software Ltd.")]
+[assembly: AssemblyProduct("Marval MSM - Custom Integration Module")]
+[assembly: AssemblyCopyright("Copyright 2017 Marval Software Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c015341d-6d2a-4f03-a4bb-1ed90da129f9")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
