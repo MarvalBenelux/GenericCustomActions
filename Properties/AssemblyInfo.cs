d091612bc7234173edc2e831f64dea6e79dbbbf0
Jasper Boot <jasper.boot@marval-benelux.nl>
2018-10-08 20:26:14 +0200

    Fixed dangling ref to shared GlobalAssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -17,3 +16,12 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fcdd2b07-5496-406e-8f6d-35cf3d11fe7c")]
+
+[assembly: AssemblyCompany("Marval Software Ltd")]
+[assembly: AssemblyProduct("Marval Service Management")]
+[assembly: AssemblyCopyright("Copyright Marval Software Ltd 2007-2018")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
